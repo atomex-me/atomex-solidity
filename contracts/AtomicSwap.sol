a683746142756cf42780fbf7072b155d7f2ef020
Alex Coon <belyga288@gmail.com>
2020-04-29 17:52:44 +0300

    refactoring

--- contracts/AtomicSwap.sol
+++ contracts/AtomicSwap.sol
@@ -1,5 +1,6 @@
 pragma solidity ^0.5.0;
 
+// From file: openzeppelin-contracts/contracts/math/SafeMath.sol 
 library SafeMath {
     function add(uint a, uint b) internal pure returns (uint c) {
         c = a + b;
@@ -12,37 +13,36 @@ library SafeMath {
     }
 }
 
-contract Destructable {
-    address payable private owner;
+// File: openzeppelin-contracts/contracts/utils/ReentrancyGuard.sol 
+contract ReentrancyGuard {
+    bool private _notEntered;
 
-    constructor() public {
-        owner = msg.sender;
+    constructor () internal {
+        _notEntered = true;
     }
 
-    modifier isDestructable {
-        require(msg.sender == owner, "only owner");
-        require(address(this).balance == 0, "balance is not zero");
+    modifier nonReentrant() {
+        require(_notEntered, "ReentrancyGuard: reentrant call");
+        _notEntered = false;
         _;
-    }
-
-    function destruct() public isDestructable {
-        selfdestruct(owner);
+        _notEntered = true;
     }
 }
 
-contract AtomicSwap is Destructable {
-    using SafeMath for uint;
+contract AtomicSwap is ReentrancyGuard {
+    using SafeMath for uint256;
 
     enum State { Empty, Initiated, Redeemed, Refunded }
 
     struct Swap {
         bytes32 hashedSecret;
-        bytes32 secret;
         address payable initiator;
         address payable participant;
-        uint refundTimestamp;
-        uint value;
-        uint payoff;
+        uint256 refundTimestamp;
+        uint256 countdown;
+        uint256 value;
+        uint256 payoff;
+        bool active;
         State state;
     }
 
@@ -50,15 +50,21 @@ contract AtomicSwap is Destructable {
         bytes32 indexed _hashedSecret,
         address indexed _participant,
         address _initiator,
-        uint _refundTimestamp,
-        uint _value,
-        uint _payoff
+        uint256 _refundTimestamp,
+        uint256 _countdown,
+        uint256 _value,
+        uint256 _payoff,
+        bool _active
     );
 
     event Added(
         bytes32 indexed _hashedSecret,
         address _sender,
         uint _value
+    );
+
+    event Activated(
+        bytes32 indexed _hashedSecret
     );
 
     event Redeemed(
@@ -72,14 +78,16 @@ contract AtomicSwap is Destructable {
 
     mapping(bytes32 => Swap) public swaps;
 
-    modifier isRefundable(bytes32 _hashedSecret) {
-        require(block.timestamp >= swaps[_hashedSecret].refundTimestamp, "refundTimestamp has not passed");
+    modifier onlyByInitiator(bytes32 _hashedSecret) {
+        require(msg.sender == swaps[_hashedSecret].initiator, "sender is not the initiator");
         _;
     }
 
-    modifier isRedeemable(bytes32 _hashedSecret, bytes32 _secret) {
-        require(block.timestamp < swaps[_hashedSecret].refundTimestamp, "refundTimestamp has already passed");
-        require(sha256(abi.encodePacked(sha256(abi.encodePacked(_secret)))) == _hashedSecret, "secret is not correct");
+    modifier isInitiatable(bytes32 _hashedSecret, address _participant, uint256 _refundTimestamp, uint256 _countdown) {
+        require(_participant != address(0), "invalid participant address");
+        require(swaps[_hashedSecret].state == State.Empty, "swap for this hash is already initiated");
+        require(block.timestamp < _refundTimestamp, "refundTimestamp has already come");
+        require(_countdown < _refundTimestamp, "countdown exceeds the refundTimestamp");
         _;
     }
 
@@ -88,40 +96,60 @@ contract AtomicSwap is Destructable {
         _;
     }
 
-    modifier isInitiatable(bytes32 _hashedSecret, uint _refundTimestamp) {
-        require(swaps[_hashedSecret].state == State.Empty, "swap for this hash is already initiated");
-        require(_refundTimestamp > block.timestamp, "refundTimestamp has already passed");
-        _;
-    }
-
     modifier isAddable(bytes32 _hashedSecret) {
-        require(block.timestamp <= swaps[_hashedSecret].refundTimestamp, "refundTime has already come");
+        require(block.timestamp < swaps[_hashedSecret].refundTimestamp, "refundTimestamp has already come");
         _;
     }
 
-    function initiate (bytes32 _hashedSecret, address payable _participant, uint _refundTimestamp, uint _payoff)
-        public payable isInitiatable(_hashedSecret, _refundTimestamp)
+    modifier isActivated(bytes32 _hashedSecret) {
+        require(swaps[_hashedSecret].active, "swap is not active");
+        _;
+    }    
+    
+    modifier isNotActivated(bytes32 _hashedSecret) {
+        require(!swaps[_hashedSecret].active, "swap is already activated");
+        _;
+    }
+
+    modifier isRedeemable(bytes32 _hashedSecret, bytes32 _secret) {
+        require(block.timestamp < swaps[_hashedSecret].refundTimestamp, "refundTimestamp has already come");
+        require(sha256(abi.encodePacked(sha256(abi.encodePacked(_secret)))) == _hashedSecret, "secret is not correct");
+        _;
+    }
+
+    modifier isRefundable(bytes32 _hashedSecret) {
+        require(block.timestamp >= swaps[_hashedSecret].refundTimestamp, "refundTimestamp has not come");
+        _;
+    }
+
+
+   function initiate (bytes32 _hashedSecret, address payable _participant, uint _refundTimestamp, uint _countdown, uint _payoff, bool _active)
+        public payable nonReentrant isInitiatable(_hashedSecret, _participant, _refundTimestamp, _countdown)
     {
         swaps[_hashedSecret].value = msg.value.sub(_payoff);
         swaps[_hashedSecret].hashedSecret = _hashedSecret;
-        swaps[_hashedSecret].initiator = msg.sender;
         swaps[_hashedSecret].participant = _participant;
+        swaps[_hashedSecret].initiator = msg.sender;
         swaps[_hashedSecret].refundTimestamp = _refundTimestamp;
+        swaps[_hashedSecret].countdown = _countdown;
         swaps[_hashedSecret].payoff = _payoff;
+        swaps[_hashedSecret].active = _active;
         swaps[_hashedSecret].state = State.Initiated;
 
         emit Initiated(
             _hashedSecret,
-            swaps[_hashedSecret].participant,
+            _participant,
             msg.sender,
-            swaps[_hashedSecret].refundTimestamp,
-            swaps[_hashedSecret].value,
-            swaps[_hashedSecret].payoff
+            _refundTimestamp,
+            _countdown,
+            msg.value.sub(_payoff),
+            _payoff,
+            _active
         );
     }
 
     function add (bytes32 _hashedSecret)
-        public payable isInitiated(_hashedSecret) isAddable(_hashedSecret)
+        public payable nonReentrant isInitiated(_hashedSecret) isAddable(_hashedSecret)
     {
         swaps[_hashedSecret].value = swaps[_hashedSecret].value.add(msg.value);
 
@@ -132,10 +160,19 @@ contract AtomicSwap is Destructable {
         );
     }
 
-    function redeem(bytes32 _hashedSecret, bytes32 _secret)
-        public isInitiated(_hashedSecret) isRedeemable(_hashedSecret, _secret)
+    function activate (bytes32 _hashedSecret)
+        public nonReentrant isInitiated(_hashedSecret) isNotActivated(_hashedSecret) onlyByInitiator(_hashedSecret)
     {
-        swaps[_hashedSecret].secret = _secret;
+        swaps[_hashedSecret].active = true;
+
+        emit Activated(
+            _hashedSecret
+        );
+    }
+
+    function redeem(bytes32 _hashedSecret, bytes32 _secret)
+        public nonReentrant isInitiated(_hashedSecret) isRedeemable(_hashedSecret, _secret)
+    {
         swaps[_hashedSecret].state = State.Redeemed;
 
         emit Redeemed(
---
+++ truffle-config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  networks: {
+    "dev_truffle": {
+      host: "127.0.0.1",
+      port: 9545,
+      network_id: "*" // Match any network id
+    },
+    "dev_ganache": {
+      host: "127.0.0.1",
+      port: 8545,
+      network_id: "*" // Match any network id
+    }  
+  }
+};
