2daf56d16c10ead0adec486e83886288f4f560c2
m-kus <mz@baking-bad.org>
2020-09-22 21:16:02 +0300

    mainnet shortcuts

--- truffle-config.js
+++ truffle-config.js
@@ -14,10 +14,14 @@ module.exports = {
       network_id: "*" // Match any network id
     },
     "ropsten": {
-      provider: () => new HDWalletProvider(process.env.MNEMONIC, `https://ropsten.infura.io/v3/${process.env.INFURA_ROPSTEN_TOKEN}`),
+      provider: () => new HDWalletProvider(process.env.MNEMONIC_ROPSTEN, `https://ropsten.infura.io/v3/${process.env.INFURA_TOKEN}`),
       network_id: 3,
       gas: 5500000,
       from: "0xd88d71de7e0544e3227785ae16d39b3623e9d90d"
+    },
+    "mainnet": {
+      provider: () => new HDWalletProvider(process.env.MNEMONIC_MAINNET, `https://mainnet.infura.io/v3/${process.env.INFURA_TOKEN}`),
+      network_id: 1
     }
   },
   plugins: ["verify-on-etherscan"]
