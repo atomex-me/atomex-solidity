3118da1acfd2ed7258bc9fbf5b5d64e7cb52dc0d
A1exCoon <belyga288@gmail.com>
2021-05-06 18:42:05 +0300

    fix

--- truffle-config.js
+++ truffle-config.js
@@ -1,5 +1,5 @@
 require('dotenv').config();
-var HDWalletProvider = require("truffle-hdwallet-provider");
+const PrivateKeyProvider = require("truffle-privatekey-provider");
 
 const Web3 = require("web3");
 const web3 = new Web3();
@@ -17,17 +17,17 @@ module.exports = {
       network_id: "*" // Match any network id
     },
     "ropsten": {
-      provider: () => new HDWalletProvider(process.env.MNEMONIC_ROPSTEN, `https://ropsten.infura.io/v3/${process.env.INFURA_TOKEN}`),
+      provider: () => new PrivateKeyProvider(process.env.PRIVATE_KEY_ROPSTEN, `https://ropsten.infura.io/v3/${process.env.INFURA_TOKEN}`),  
       network_id: 3,
       gas: 5500000,
-      from: "0xd88d71de7e0544e3227785ae16d39b3623e9d90d"
+      from: process.env.ADDRESS_ROPSTEN
     },
     "mainnet": {
-      provider: () => new HDWalletProvider(process.env.MNEMONIC_MAINNET, `https://mainnet.infura.io/v3/${process.env.INFURA_TOKEN}`),
+      provider: () => new PrivateKeyProvider(process.env.PRIVATE_KEY_MAINNET, `https://mainnet.infura.io/v3/${process.env.INFURA_TOKEN}`),
       gasPrice: web3.utils.toWei('103', 'gwei'),
       gas: 2000000,
       network_id: 1,
-      from: '0xF6E592dfC4445dC3536fa94de33b8A5E1071CAAa'
+      from: process.env.ADDRESS_MAINNET
     }
   },
   compilers: {
